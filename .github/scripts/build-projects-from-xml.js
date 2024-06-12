3ae0373cf50008a9c30c804013d4da383046d18f
aegilops <41705651+aegilops@users.noreply.github.com>
2024-06-12 15:42:09 +0100

    warn on missing variables

--- .github/scripts/build-projects-from-xml.js
+++ .github/scripts/build-projects-from-xml.js
@@ -11,8 +11,7 @@ function run(github, context, core) {
   }
 
   if (!variables) {
-    if (core) { core.setFailed("variables is required"); }
-    return;
+    if (core) { core.warn("Setting the 'variables' inputs is recommended, to properly resolve paths in the build XML that are substituted with a variable of the form $(VAR)"); }
   }
 
   const parsed_variables = variables ? yaml.parse(variables) : {};
